cd9536b4a1359942d671a56c6abaa313258701f8
Ramon Saraiva <ramonsaraiva@gmail.com>
2018-09-01 12:11:34 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+import os
+
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(
+    name='django-expiry',
+    version='0.1.0',
+    description='Expiry rules for Django sessions.',
+    long_description=read('README.md'),
+    long_description_content_type='text/markdown',
+    author='Ramon Saraiva',
+    author_email='ramonsaraiva@gmail.com',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    install_requires=[],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
